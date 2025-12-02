651c1cce37cf6c6ed1dfcc2027c70a8cab5493b5
Jonathan Ebert <ebert.johny@gmail.com>
2025-12-02 22:18:25 +0100

    Update .gitignore and modify CMakeLists to build tag2 executable

--- tag2/main.cpp
+++ tag2/main.cpp
@@ -1,14 +1,16 @@
 #include <print>
 #include <sstream>
+#include <filesystem>
 
 #include "types.hpp"
 #include "aoc_helper.cpp"
 
 int main()
 {
-    auto raw_input{read_file("test.txt")};
-    std::println("{}", raw_input);
-    // Split input on commas
+    std::filesystem::path p{"test.txt"};
+    auto raw_input{read_file(p)};
+
+    // Split input into pairs
     vector<pair<string, string>> data{};
     size_t last{0};
     while (true)
@@ -23,15 +25,33 @@ int main()
         if (comma == string::npos)
         {   
             pair<string, string> ids = {raw_input.substr(last+1, minus-last-1), raw_input.substr(minus+1)};
-            std::println("{}, {}", ids.first, ids.second);
             data.push_back(ids);
             break;
         }
         pair<string, string> ids = {raw_input.substr(last+1, minus-last-1), raw_input.substr(minus+1, comma-minus-1)};
-        std::println("{}, {}", ids.first, ids.second);
         data.push_back(ids);
         last = comma;
     }
 
+    // Print pairs
+    for (const auto& [id1, id2] : data)
+    {
+        std::println("{}, {}", id1, id2);
+    }
+
+
+    for (const auto& [id1, id2] : data)
+    {
+        if (id1.size()%2 && id2.size()%2)
+        {
+            continue;
+        }
+        size_t half1 {id1.size()/2};
+        size_t half2 {id2.size()/2};
+        std::println("{}, {}", half1, half2);
+    }
+
     return 0;
 }
+
+// cd ../build && cmake --build . && cp tag2.out ../tag2/tag2.out && cd ../tag2 && ./tag2.out
